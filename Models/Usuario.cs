19608b979b337cf9a341209f03dcb2335dd3393d
André Solér <128252837+andre-soler@users.noreply.github.com>
2025-06-07 00:16:48 -0300

    Add files via upload

---
+++ Models/Usuario.cs
@@ -0,0 +1,19 @@
+﻿namespace MonitoramentoApagao.Models
+{
+    public class Usuario
+    {
+        public string Nome { get; set; }
+        public string Senha { get; set; }
+
+        public Usuario(string nome, string senha)
+        {
+            Nome = nome;
+            Senha = senha;
+        }
+
+        public bool Autenticar(string senhaDigitada)
+        {
+            return Senha == senhaDigitada;
+        }
+    }
+}
